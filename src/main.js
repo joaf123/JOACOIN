c708e150a09fd4d6da110859bd0478f37ba2371d
Joachim Fosse <joachim.fosse@aksjeservice.no>
2021-09-10 22:17:03 +0200

    Added Transaction Signing

--- src/main.js
+++ src/main.js
@@ -1,117 +1,27 @@
-const SHA256 = require('crypto-js/sha256');
+const {BlockChain, Transaction} = require('./blockchain')
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
 
-class Transaction{
-    constructor(fromAddress, toAddress, amount) {
-        this.fromAddress = fromAddress;
-        this.toAddress = toAddress;
-        this.amount = amount;
-    }
-}
-
-class Block {
-    constructor(timestamp, transactions, previousHash = ''){        
-        this.previousHash = previousHash;
-        this.timestamp = timestamp;
-        this.transactions = transactions;
-        this.hash = this.calculateHash();
-        this.nonce = 0;
-    }
-
-    calculateHash(){
-        return SHA256(this.previousHash + this.timestamp + JSON.stringify(this.transactions) + this.nonce).toString();
-    }
-
-    mineBlock(difficulty){
-        while(this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")){
-            this.nonce++;
-            this.hash = this.calculateHash();
-        }
-
-        console.log("BLOCK MINED: " + this.hash);
-    }
-}
-
-
-class BlockChain{
-    constructor(){
-        this.chain = [this.createGenesisBlock()];
-        this.difficulty = 2;
-        this.pendingTransactions = [];
-        this.miningReward = 100;
-    }
-
-    createGenesisBlock(){
-        return new Block("01.01.2021", "Genesis block", "0");
-    }
-
-    getLatestBlock(){
-        return this.chain[this.chain.length - 1];
-    }
-
-    minePendingTransactions(miningRewardAdress){
-        let block = new Block(Date.now(), this.pendingTransactions, this.chain[this.chain.length - 1].hash);
-        block.mineBlock(this.difficulty);
-
-        console.log('Block successfully mined!');
-        this.chain.push(block);
-
-        this.pendingTransactions = [
-            new Transaction(null, miningRewardAdress, this.miningReward)
-        ];
-    }
-
-    createTransaction(transaction){
-        this.pendingTransactions.push(transaction);
-    }
-
-    getBalanceOfAdress(address){
-        let balance = 0;
-
-        for(const block of this.chain){
-            for(const trans of block.transactions){
-                if(trans.fromAddress === address){                    
-                    balance -= trans.amount;
-                }
-
-                if(trans.toAddress === address){                    
-                    balance += trans.amount;
-                    
-                }
-            }                    
-        }
-
-        
-        return balance;
-    }
-
-    isChainValid(){
-        for(let i= 1; i < this.chain.length; i++) {
-            const currentBlock = this.chain[i];
-            const previousBlock = this.chain[i - 1];
-
-            if(currentBlock.hash !== currentBlock.calculateHash()){
-                return false;
-            }
-
-            if(currentBlock.previousHash !== previousBlock.hash){
-                return false;
-            }
-        }
-
-        return true;
-    }
-}
+const myKey = ec.keyFromPrivate('f3db751dbc48302569f402ace02ec5ae5669e89eeb210a57ece84cc5cb4f88a9');
+const myWalletAddress = myKey.getPublic('hex');
 
 let joaCoin = new BlockChain();
-joaCoin.createTransaction(new Transaction('adress1', 'adress2', 100));
-joaCoin.createTransaction(new Transaction('adress2', 'adress1', 50));
+
+const tx1 = new Transaction(myWalletAddress, 'Public key goes here', 10);
+tx1.signTransaction(myKey);
+joaCoin.addTransaction(tx1);
 
 console.log('\n Starting the miner...');
-joaCoin.minePendingTransactions('joachim-adress');
+console.log('Previous hash: ', joaCoin.getLatestBlock().previousHash)
+console.log('Hash: ', joaCoin.getLatestBlock().hash)
+joaCoin.minePendingTransactions(myWalletAddress);
+console.log('Previous hash: ', joaCoin.getLatestBlock().previousHash)
 
-console.log('\n Balance of Joachim is: ', joaCoin.getBalanceOfAdress('joachim-adress'));
+// Mine pending again to get the mining reward
+console.log('Hash: ', joaCoin.getLatestBlock().hash)
+joaCoin.minePendingTransactions(myWalletAddress);
+console.log('Previous hash: ', joaCoin.getLatestBlock().previousHash)
 
-console.log('\nStarting the miner again...');
-joaCoin.minePendingTransactions('joachim-adress');
+console.log('\n Balance of Joachim is: ', joaCoin.getBalanceOfAddress(myWalletAddress));
 
-console.log('\n Balance of Joachim is: ', joaCoin.getBalanceOfAdress('joachim-adress'));
+console.log('Is Chain Valid?: ', joaCoin.isChainValid());
